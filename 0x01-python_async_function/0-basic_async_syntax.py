a974cd8deb0e34b59f1bd49025d718a52c36c1b0
Anthony Kanu <kanuchibueze@gmail.com>
2023-07-11 23:21:15 +0000

    tak 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,6 @@ async def wait_random(max_delay: int = 0)-> float:
     """ a function that awaits for a random
         routinten and returns it
     """
-    delay = random.random() * max_delay
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
