d6fcb1cd57d11450a9d9c4bd22b9ea60d3f17ed0
Anthony Kanu <kanuchibueze@gmail.com>
2023-07-13 03:45:34 +0000

    task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr?bin/env python3
+"""task 2
+"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """return the runtime of running a funct 4 times using asyncio.gather
+    """
+    star_time = time.time()
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+    )
+
+    return time.time() - start_time
