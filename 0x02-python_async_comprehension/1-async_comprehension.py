29b2b78487b7f1a233b669c0370f541af583b2f1
Anthony Kanu <kanuchibueze@gmail.com>
2023-07-13 03:15:01 +0000

    task 2

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""task 1
+"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """generates a list of random number from another function
+    """
+
+    return [x async for x in async_generator())
